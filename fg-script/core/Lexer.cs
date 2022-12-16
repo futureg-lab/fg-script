150cc71c58ee62ab4f0bb652f49ad191564f9f1d
Michaël <afmichael73@gmail.com>
2022-12-17 00:03:35 +0300

    fix : indent

--- fg-script/core/Lexer.cs
+++ fg-script/core/Lexer.cs
@@ -2,177 +2,177 @@
 {
     public class Lexer
     {
-		protected char? CurrentChar { get; set; } = null;
-		protected string Source { get; set; } = "";
-		protected string Filepath { get; set; } = "";
-		protected CursorPosition Cursor = new(0, 0, -1);
+        protected char? CurrentChar { get; set; } = null;
+        protected string Source { get; set; } = "";
+        protected string Filepath { get; set; } = "";
+        protected CursorPosition Cursor = new(0, 0, -1);
 
-		public List<Token> Tokenize()
+        public List<Token> Tokenize()
         {
-			List<Token> tokens = new();
-			NextChar(); // -1 + 1 = 0
-			while (!HasEnded())
+            List<Token> tokens = new();
+            NextChar(); // -1 + 1 = 0
+            while (!HasEnded())
             {
-				string lexeme = ""+ CurrentChar;
-				if (lexeme == " ")
+                string lexeme = "" + CurrentChar;
+                if (lexeme == " ")
                 {
-					NextChar();
-					continue;
-				}
-
-				if (lexeme.StartsWith("\""))
-                {
-					string str = MakeString();
-					Token token = new(str, TokenType.STRING, Cursor.Copy());
-					tokens.Add(token);
-					continue;
-				}
-				
-				if(IsNum(CurrentChar))
-                {
-					string str = MakeNumber();
-					Token token = new(str, TokenType.NUMBER, Cursor.Copy());
-					tokens.Add(token);
-					continue;
-				}
-				
-				// has to be alphanum + _
-				if(IsAlpha(CurrentChar))
-                {
-					string str = MakeStandardExpression();
-					Token token = new(str, TokenType.KEYWORD_OR_NAME, Cursor.Copy());
-					tokens.Add(token);
-					continue;
-				}
-				
-                {
-					Token token = new(lexeme, TokenType.UNKNOWN, Cursor.Copy());
-					tokens.Add(token);
-					NextChar();
-				}
-			}
-			tokens.Add(new("EOF", TokenType.EOF, Cursor.Copy()));
-			return tokens;
-        }
-
-		//
-		// FGScript specific token eaters
-		//
-
-
-		//
-		// Basic token eaters
-		//
-
-		// standard string : "hello world!"
-		protected string MakeString()
-        {
-			string str = "";
-			NextChar(); // ignore first '"'
-			while (CurrentChar != '"')
-            {
-				str += CurrentChar;
-				NextChar();
-				if (HasEnded())
-					throw new SyntaxErrorException("interminated string", Cursor.Copy(), Filepath);
-            }
-			NextChar(); // ignore '"'
-			return str;
-        }
-
-		// [0-9]+
-		protected string MakeNumber()
-        {
-			string str = "";
-			bool dotEncountered = false;
-			while (IsNum(CurrentChar) || CurrentChar == '.')
-            {
-				if (CurrentChar == '.')
-                {
-					if (dotEncountered)
-						throw new SyntaxErrorException("invalid number", Cursor.Copy(), Filepath);
-					dotEncountered = true;
+                    NextChar();
+                    continue;
                 }
-				str += CurrentChar;
-				NextChar();
+
+                if (lexeme.StartsWith("\""))
+                {
+                    string str = MakeString();
+                    Token token = new(str, TokenType.STRING, Cursor.Copy());
+                    tokens.Add(token);
+                    continue;
+                }
+
+                if (IsNum(CurrentChar))
+                {
+                    string str = MakeNumber();
+                    Token token = new(str, TokenType.NUMBER, Cursor.Copy());
+                    tokens.Add(token);
+                    continue;
+                }
+
+                // has to be alphanum + _
+                if (IsAlpha(CurrentChar))
+                {
+                    string str = MakeStandardExpression();
+                    Token token = new(str, TokenType.KEYWORD_OR_NAME, Cursor.Copy());
+                    tokens.Add(token);
+                    continue;
+                }
+
+                {
+                    Token token = new(lexeme, TokenType.UNKNOWN, Cursor.Copy());
+                    tokens.Add(token);
+                    NextChar();
+                }
             }
-			return str;
+            tokens.Add(new("EOF", TokenType.EOF, Cursor.Copy()));
+            return tokens;
         }
 
-		// [a-zA-Z0-9_]+
-		protected string MakeStandardExpression()
-        {
-			string str = "";
-			while (IsStandardExpression(CurrentChar))
-			{
-				str += CurrentChar;
-				NextChar();
-			}
-			return str;
-		}
+        //
+        // FGScript specific token eaters
+        //
 
-		public Lexer(string source, string filepath = "<console>")
-        {
-			Source = source;
-			Filepath = filepath;
-		}
-		
-		// [0-9]
-		public static bool IsNum(char? character)
-        {
-			if (character == null)
-				return false;
-			return character >= '0' && character <= '9';
-        }
 
-		// [a-zA-Z]
-		public static bool IsAlpha(char? character)
-        {
-			if (character == null)
-				return false;
-			return character >= 'a' && character <= 'z' 
-				|| character >= 'A' && character <= 'Z';
-        }
+        //
+        // Basic token eaters
+        //
 
-		public static bool IsAlphaNum(char? character)
-		{
-			return IsAlpha(character) || IsNum(character);
-		}
-
-		// 
-		public static bool IsStandardExpression(char? character)
+        // standard string : "hello world!"
+        protected string MakeString()
         {
-			return IsAlpha(character) || character == '_';
-        }
-
-		public void NextChar()
-        {
-			Cursor.Pos++;
-			if (Cursor.Pos >= Source.Length)
-				CurrentChar = null;
-			else
-				CurrentChar = Source[Cursor.Pos];
-			
-			if (CurrentChar == '\n')
+            string str = "";
+            NextChar(); // ignore first '"'
+            while (CurrentChar != '"')
             {
-				Cursor.Line++;
-				Cursor.Col = 0;
-				NextChar();
+                str += CurrentChar;
+                NextChar();
+                if (HasEnded())
+                    throw new SyntaxErrorException("interminated string", Cursor.Copy(), Filepath);
+            }
+            NextChar(); // ignore '"'
+            return str;
+        }
+
+        // [0-9]+
+        protected string MakeNumber()
+        {
+            string str = "";
+            bool dotEncountered = false;
+            while (IsNum(CurrentChar) || CurrentChar == '.')
+            {
+                if (CurrentChar == '.')
+                {
+                    if (dotEncountered)
+                        throw new SyntaxErrorException("invalid number", Cursor.Copy(), Filepath);
+                    dotEncountered = true;
+                }
+                str += CurrentChar;
+                NextChar();
+            }
+            return str;
+        }
+
+        // [a-zA-Z0-9_]+
+        protected string MakeStandardExpression()
+        {
+            string str = "";
+            while (IsStandardExpression(CurrentChar))
+            {
+                str += CurrentChar;
+                NextChar();
+            }
+            return str;
+        }
+
+        public Lexer(string source, string filepath = "<console>")
+        {
+            Source = source;
+            Filepath = filepath;
+        }
+
+        // [0-9]
+        public static bool IsNum(char? character)
+        {
+            if (character == null)
+                return false;
+            return character >= '0' && character <= '9';
+        }
+
+        // [a-zA-Z]
+        public static bool IsAlpha(char? character)
+        {
+            if (character == null)
+                return false;
+            return character >= 'a' && character <= 'z'
+                || character >= 'A' && character <= 'Z';
+        }
+
+        public static bool IsAlphaNum(char? character)
+        {
+            return IsAlpha(character) || IsNum(character);
+        }
+
+        // 
+        public static bool IsStandardExpression(char? character)
+        {
+            return IsAlpha(character) || character == '_';
+        }
+
+        public void NextChar()
+        {
+            Cursor.Pos++;
+            if (Cursor.Pos >= Source.Length)
+                CurrentChar = null;
+            else
+                CurrentChar = Source[Cursor.Pos];
+
+            if (CurrentChar == '\n')
+            {
+                Cursor.Line++;
+                Cursor.Col = 0;
+                NextChar();
             }
             else
             {
-				Cursor.Col++;
+                Cursor.Col++;
             }
         }
 
-		public char? PeekNextChar()
+        public char? PeekNextChar()
         {
-			return Cursor.Pos + 1 >= Source.Length ? null : Source[Cursor.Pos + 1];
-		}
-
-		public bool HasEnded()
-        {
-			return CurrentChar == null;
+            return Cursor.Pos + 1 >= Source.Length ? null : Source[Cursor.Pos + 1];
         }
-	}
+
+        public bool HasEnded()
+        {
+            return CurrentChar == null;
+        }
+    }
 }
