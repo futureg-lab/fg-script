67d3fc00a7522f1eef379344ed622920f7fb027c
Michaël <afmichael73@gmail.com>
2023-01-05 00:11:41 +0300

    fix : underline text

--- fg-script/utils/Utils.cs
+++ fg-script/utils/Utils.cs
@@ -18,13 +18,23 @@ namespace fg_script.utils
 
         public static string UnderlineText(string text, int start, int end)
         {
+            if (start > end)
+                throw new Exception("start should be less than end");
             start = Math.Max(0, start);
-            end = Math.Min(text.Length, end);
-            int initialLength = text.Length;
+            int initialLength = Math.Max(end, text.Length);
             text += "\n";
             for (int i = 0; i < initialLength; i++)
-                text += (i >= start && i < end ? "^" : " ");
+            {
+                text += (i >= start && i < end ? "^" : "-");
+            }
             return text + "\n";
+        }
+
+        public static string UnderlineTextLine(string text, CursorPosition pos, int offset = 0)
+        {
+            string[] splits = text.Split("\n");
+            string line = splits[pos.Line - 1];
+            return UnderlineText(line, pos.Col - 2 + offset, pos.Col - 1 + offset);
         }
     }
 }
